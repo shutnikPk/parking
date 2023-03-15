5401cca02f120e9615acf580aec517b3d8bae84f
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2023-03-15 23:06:56 +0300

    almost done

--- src/components/First/First.jsx
+++ src/components/First/First.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import './First.scss'
 function First() {
     return (
-        <div className="div">            
-            {process.env.NODE_ENV === 'production' ? <div className="first">First Component in {process.env.NODE_ENV} mode</div> : <div className="first">First Component in {process.env.NODE_ENV} mode</div>}
+        <div className="div">
+            {
+                process.env.NODE_ENV === 'production' ?
+                    <div className="first">First Component in {process.env.NODE_ENV} mode</div>
+                    : <div className="first">First Component in {process.env.NODE_ENV} mode</div>
+            }
         </div>
     );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,70 @@
-const webpack = require('webpack');
+const { DefinePlugin } = require('webpack');
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = env=>{
-  const port= env.port||4000
-  const mode = env.mode||'development'
-  const isProd = process.env.NODE_ENV === 'production'
+module.exports = env => {
+  const PORT = env.PORT || 4000
+  const NODE_ENV = env.MODE ? env.MODE : 'development'
+  const isProd = NODE_ENV === 'production'
   const isDev = !isProd
-  return{
+  return {
     context: path.resolve(__dirname, 'src'),
-    mode:mode,
+    mode: NODE_ENV,
     entry: './index.jsx',
     output: {
-        filename: isDev? 'bundle.[hash].js': 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
+      filename: isDev ? 'bundle.[hash].js' : 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      assetModuleFilename: isDev ? 'assets/[hash][ext]' : 'assets/[name][ext]',
+      clean: true,
     },
-    resolve:{
-        extensions:['.js', '.jsx', '.json'],
+    resolve: {
+      extensions: ['.js', '.jsx', '.json'],
     },
-    devServer:{
-      port: port,
+    devServer: {
+      port: PORT,
       hot: isDev
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template:'index.html',
-            title: 'Parking',
-        }), 
-          new webpack.EnvironmentPlugin({
-            'PORT': port,
-            'NODE_ENV': mode,
-          }),
-          new MiniCssExtractPlugin()
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+        title: 'Parking',
+      }),
+      new DefinePlugin({
+        'process.env': JSON.stringify({
+          'PORT': PORT,
+          'NODE_ENV': NODE_ENV,
+        })
+      }),
+      new MiniCssExtractPlugin()
     ],
     module: {
-        rules: [
-            {
-              test:/\.(s*)css$/,
-              use: [
-                MiniCssExtractPlugin.loader,
-                'css-loader',
-                'sass-loader',
-              ],
-            },
-            {
-                test: /\.js$|jsx/,
-                exclude: /(node_modules|bower_components)/,
-                use:  {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: [
-                      '@babel/preset-env',
-                    '@babel/preset-react'
-                  ]
-                  }
-                }
-              },
-              {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-              }
-          ],
+      rules: [{
+        test: /\.(s*)css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
       },
-}}
+      {
+        test: /\.js$|jsx/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+      ],
+    },
+  }
+}
