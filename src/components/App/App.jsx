d44a8519d7bd5b964d878d9a9c38238a7ae30b66
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2023-03-24 00:24:58 +0300

    add cont 'theme'

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,19 +3,22 @@ import React, { useState } from 'react';
 import Sun from './sun.svg'
 import Moon from './half-moon.svg'
 
+const theme = localStorage.getItem('isLightTheme') === null ?
+    true : JSON.parse(localStorage.getItem('isLightTheme'))
+
 function App() {
 
-    const [isLightTheme, setIsLightTheme] = useState(JSON.parse(localStorage.getItem("isLightTheme")) && true)
+    const [isLightTheme, setIsLightTheme] = useState(theme)
 
     const onChangeTheme = () => {
-        localStorage.setItem("isLightTheme", !isLightTheme)
+        localStorage.setItem('isLightTheme', !isLightTheme)
         setIsLightTheme(!isLightTheme)
     }
 
     return (
         <div className={`theme-container theme__${isLightTheme ? 'light' : 'dark'}`} >
             {isLightTheme ? 'Good Morgin' : 'Good Night'}
-            <span className='theme--icon'>{isLightTheme ? <Sun /> : <Moon className={"moon"} />}</span>
+            <span className='theme--icon'>{isLightTheme ? <Sun /> : <Moon className='moon' />}</span>
             <input checked={isLightTheme}
                 type='checkbox'
                 onChange={onChangeTheme}>
