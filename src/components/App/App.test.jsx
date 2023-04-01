7cc3d9a89efc0878daf512e0cc55b061e3544d8a
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2023-04-01 23:15:53 +0300

    add eslint config

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,33 +1,33 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
-import App from './App.jsx'
+import App from './App';
 
 describe('App', () => {
-    it('render App', () => {
-        render(<App />)
-        expect(screen.getByText(/hello world!/i)).toBeInTheDocument()
-    })
-})
+  it('render App', () => {
+    render(<App />);
+    expect(screen.getByText(/hello world!/i)).toBeInTheDocument();
+  });
+});
 
 describe('events', () => {
-    it('checkbox click', () => {
-        render(<App />)
-        const checkbox = screen.getByRole('checkbox')
-        expect(checkbox).toBeChecked()
-        expect(screen.getByText(/Good Morgin/i)).toBeInTheDocument()
-        fireEvent.click(checkbox)
-        expect(checkbox).not.toBeChecked()
-        expect(screen.getByText(/Good Night/i)).toBeInTheDocument()
-    })
-})
+  it('checkbox click', () => {
+    render(<App />);
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox).toBeChecked();
+    expect(screen.getByText(/Good Morgin/i)).toBeInTheDocument();
+    fireEvent.click(checkbox);
+    expect(checkbox).not.toBeChecked();
+    expect(screen.getByText(/Good Night/i)).toBeInTheDocument();
+  });
+});
 
 describe('change theme', () => {
-    it('change background colors', () => {
-        render(<App />)
-        const container = screen.getByRole('container')
-        const checkbox = screen.getByRole('checkbox')
-        expect(container.classList.contains('theme__light'))
-        fireEvent.click(checkbox)
-        expect(container.classList.contains('theme__light'))
-    })
-})
+  it('change background colors', () => {
+    render(<App />);
+    const container = screen.getByRole('main');
+    const checkbox = screen.getByRole('checkbox');
+    expect(container.classList.contains('theme__light')).toBe(true);
+    fireEvent.click(checkbox);
+    expect(container.classList.contains('theme__light')).toBe(true);
+  });
+});
